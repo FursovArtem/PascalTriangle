e8decba237f1dac53b7fc866eeac473554863575
aztec <aztec@DESKTOP-CK171NA>
2023-04-25 18:04:00 +0400

    Add project files.

---
+++ PascalTriangle/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+string multistring(string str, int times)
+{
+	string result;
+	while (times > 0)
+	{
+		result += str;
+		times--;
+	}
+	return result;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите высоту треугольника: "; cin >> n; n--;
+	while (n < 0 || n > 20)
+	{
+		cout << "Невозможная высота треугольника. Повторите ввод: "; cin >> n; n--;
+	}
+	vector<int> v(n + 1);
+	v[0] = 1;
+	cout << multistring("   ", n) << v[0] << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i; j >= 1; j--)
+		{
+			v[j] = v[j - 1] + v[j];
+		}
+		cout << multistring("   ", n - i);
+		for (int j = 0; j <= i; j++)
+		{
+			cout << std::left << std::setw(6) << v[j];
+		}
+		cout << endl;
+	}
+}
